ea1ef8031594cc0a8e0a92156c142f844200080e
Dzakwan Irfan <lexdani368@gmail.com>
2024-03-19 21:06:55 +0700

    feat: OAuth Google & Github

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,6 +5,7 @@ import { LoginSchema } from "@/schemas";
 import { CardWrapper } from "./card-wrapper"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useSearchParams } from "next/navigation";
 import { 
     Form,
     FormControl,
@@ -21,6 +22,11 @@ import { login } from "@/actions/login";
 import { useState, useTransition } from "react";
 
 export const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+        ? "Email already in use with different provider!"
+        : "";
+
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
     const [isPending, startTransition] = useTransition();
@@ -40,8 +46,9 @@ export const LoginForm = () => {
         startTransition(() => {
             login(values)
                 .then((data) => {
-                    setError(data.error);
-                    setSuccess(data.success);
+                    setError(data?.error);
+                    // TODO: Add when we add 2FA
+                    // setSuccess(data?.success);
                 })
         });
     };
@@ -96,7 +103,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button
                         type="submit"
