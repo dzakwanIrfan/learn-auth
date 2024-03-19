885b8a1a2eb07890579b520d9d6233c0ccb936fd
Dzakwan Irfan <lexdani368@gmail.com>
2024-03-20 05:36:54 +0700

    feat: Resend (Sending Email)

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,8 @@ import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
@@ -30,7 +32,11 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         },
     });
 
-    // TODO: Send verification token email
+    const verificationToken = await generateVerificationToken(email);
+    await sendVerificationEmail(
+        verificationToken.email,
+        verificationToken.token
+    );
 
-    return { success: "User created!" };
+    return { success: "Confirmation email sent!" };
 }
