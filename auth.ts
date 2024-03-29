cc9238a03bcac2a909b9cf86ddca368d0ebf2c60
Dzakwan Irfan <lexdani368@gmail.com>
2024-03-29 08:38:47 +0700

    feat: Server & Client session

--- auth.ts
+++ auth.ts
@@ -53,7 +53,9 @@ export const {
         if (token.sub && session.user) session.user.id = token.sub;
 
         if (token.role && session.user) session.user.role = token.role as UserRole;
-
+        
+        if (session.user) session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+        
         return session;
       },
       async jwt({token}) {
@@ -64,6 +66,7 @@ export const {
         if(!existingUser) return token;
 
         token.role = existingUser.role;
+        token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
         return token;
       }
